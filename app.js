c1ae47c5f6ae5eca46a4d3e96f61e298aefea6b9
Palak Gautam <gautampalak77@gmail.com>
2026-03-04 00:28:37 +0530

    Learning JS

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-// add new feature - form
+// console.log("Hello World");
+// console.log("Apna College");
+let a=3;
+let b=2;
+//console.log("sum is :", a+b);
+
+let output=`The total price is ${a+b} rupees`
+console.log(output);
+
+let str= "apple";
+
+if((str[0]==='a') && (str.length>3)){
+    console.log("Good String");
+}
+else{
+    console.log("Bad String");
+}
