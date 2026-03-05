99b6724993e5cd2665d99cd11928ae5d716d5694
Palak Gautam <gautampalak77@gmail.com>
2026-03-06 00:31:21 +0530

    basics of JS

--- app.js
+++ app.js
@@ -1,17 +1,88 @@
 // console.log("Hello World");
 // console.log("Apna College");
-let a=3;
-let b=2;
-//console.log("sum is :", a+b);
+// let a=3;
+// let b=2;
+// //console.log("sum is :", a+b);
 
-let output=`The total price is ${a+b} rupees`
-console.log(output);
+// let output=`The total price is ${a+b} rupees`
+// console.log(output);
 
-let str= "apple";
+// let str= "apple";
 
-if((str[0]==='a') && (str.length>3)){
-    console.log("Good String");
-}
-else{
-    console.log("Bad String");
+// if((str[0]==='a') && (str.length>3)){
+//     console.log("Good String");
+// }
+// else{
+//     console.log("Bad String");
+// }
+
+// 
+
+
+// let name = prompt("please enter your name");
+// let age  = prompt("please enter your age");
+// alert(`${name} is ${age} yearsold.`);
+
+
+
+// let quarter = 1;
+
+// switch (quarter) {
+//     case 1:
+//         console.log("January, February, March");
+//         break;
+//     case 2:
+//         console.log("April, May, June");
+//         break;
+//     case 3:
+//         console.log("July, August, September");
+//         break;
+//     case 4:
+//         console.log("October, November, December");
+//         break;
+//     default:
+//         console.log("NOT A QUARTER!");
+// }
+
+
+
+// let str = "apples";
+
+// if ((str[0] == 'a' || str[0] == 'A') && (str.length > 5)) {
+//     console.log("golden string");
+// } else {
+//     console.log("not a golden string");
+// }
+
+
+
+// let a = 5;
+// let b = 18;
+// let c = 13;
+
+// if (a > b) {
+//     if (a > c) {
+//         console.log(a, "is largest");
+//     } else {
+//         console.log(c, "is largest");
+//     }
+// } else {
+//     if (b > c) {
+//         console.log(b, "is largest");
+//     } else {
+//         console.log(c, "is largest");
+//     }
+// }
+
+
+
+
+// Ans6 (Bonus)
+let num1 = 32;
+let num2 = 47852;
+
+if ((num1 % 10) == (num2 % 10)) {
+    console.log("numbers have the same last digit which is", num1 % 10);
+} else {
+    console.log("numbers don't have the same last digit");
 }
